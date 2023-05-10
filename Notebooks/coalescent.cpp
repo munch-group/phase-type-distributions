5736911d299c1c9b31c0d38098553702a2646bb5
kaspermunch <kaspermunch@birc.au.dk>
2023-05-10 15:19:39 +0200

    Updated notebooks

--- Notebooks/coalescent.cpp
+++ Notebooks/coalescent.cpp
@@ -177,14 +177,14 @@ SEXP construct_coalescent_selection_graph(int sample_size, int n_derived, int po
        fprintf(stderr, "bin %d\n", bin_center);
     }
     
-    int *bins = (int *) calloc((size_t)(n_freqbins+2), sizeof(int));
-    bins[i] = 0
-    for (int i = 1; i < n_freqbins+1; ++i) {
-       int bin_center = (int)round(pop_size*((i+1)/((double)n_freqbins) - 1/(double)(n_freqbins)/2)) ;
-       bins[i] = bin_center;
-       fprintf(stderr, "bin %d\n", bin_center);
-    }
-    bins[n_freqbins+1] = 0
+    // int *bins = (int *) calloc((size_t)(n_freqbins+2), sizeof(int));
+    // bins[i] = 0
+    // for (int i = 1; i < n_freqbins+1; ++i) {
+    //    int bin_center = (int)round(pop_size*((i+1)/((double)n_freqbins) - 1/(double)(n_freqbins)/2)) ;
+    //    bins[i] = bin_center;
+    //    fprintf(stderr, "bin %d\n", bin_center);
+    // }
+    // bins[n_freqbins+1] = 0;
     
     // int *frequencies = (int *) calloc((size_t)(n_freqbins+2), sizeof(int));
     // for (int i = 0; i < n_freqbins; ++i) {
