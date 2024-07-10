13ebc5add4da4de130be8fbd4c55c04b32b78677
Kasper Munch <kaspermunch@birc.au.dk>
2024-07-10 16:01:48 -0600

    Updated workflow.py

--- workflow.py
+++ workflow.py
@@ -1,11 +1,12 @@
 from gwf import Workflow
 
-gwf = Workflow(defaults={'account': 'davide_intern'})
+gwf = Workflow(defaults={'account': 'phasetype'})
 
-for i in range(3, 101):
+for i in range(3, 151):
 
     sentinel_file_name = f'steps/block_coal_rewards_{i}.txt'
     gwf.target(f'coal_rewards_{i}', inputs=[], outputs=[sentinel_file_name], walltime='6-23:00:00', memory = '8gb') << f"""
     mkdir -p steps
-    ~/miniconda3/envs/phasetype/bin/Rscript coal_block_rewards.r {i}
+    ~/miniconda3/envs/phasetype/bin/Rscript coal_block_rewards.r coal_block_rewards.h5 {i} && touch {sentinel_file_name}
     """
+    
