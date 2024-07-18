565933a9a4dd982f45985f557030923bd4e87ad3
Kasper Munch <kaspermunch@birc.au.dk>
2024-07-18 08:55:53 +0200

    file updates to accomodate restructuring

--- workflow.py
+++ workflow.py
@@ -7,6 +7,6 @@ for i in range(3, 151):
     sentinel_file_name = f'steps/block_coal_rewards_{i}.txt'
     gwf.target(f'coal_rewards_{i}', inputs=[], outputs=[sentinel_file_name], walltime='6-23:00:00', memory = '8gb') << f"""
     mkdir -p steps
-    ~/miniconda3/envs/phasetype/bin/Rscript coal_block_rewards.r coal_block_rewards.h5 {i} && touch {sentinel_file_name}
+    ~/miniconda3/envs/phasetype/bin/Rscript ./scripts/coal_block_rewards.r ./data/coal_block_rewards.h5 {i} && touch {sentinel_file_name}
     """
     
